172f8df8837e4cb7364f6b74786b831b6864c316
Wycliffe Odhiambo <odhiambow2354@gmail.com>
2026-06-07 21:51:10 +0300

    intrduction to class & object

---
+++ src/oop/TextClass.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class TextClass {
+    public static void main(String[] args) {
+        var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text);
+    }
+}
